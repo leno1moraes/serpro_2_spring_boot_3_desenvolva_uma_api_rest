3922a58d49fd7905961be2de1c81f9e365d74f19
leno1moraes <lenomoraes12@gmail.com>
2024-04-18 20:11:00 -0400

    projeto finalizado

--- proj/api/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ proj/api/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -27,12 +27,17 @@ public class AutenticacaoController {
     @SuppressWarnings("rawtypes")
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(authenticationToken);
-
-        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
-
-        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+        try {
+            var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+            var authentication = manager.authenticate(authenticationToken);
+    
+            var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+    
+            return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
